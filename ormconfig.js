353f2c757b8c4bd80cc5f323d77429384842d0bd
jh-lee99 <jun99h1219@gmail.com>
2024-10-30 00:45:20 +0900

    edit ormconfig 2

--- ormconfig.js
+++ ormconfig.js
@@ -4,11 +4,11 @@ const isTS = require('detect-ts-node');
 module.exports = {
   name: "default",
   type: "mysql",
-  host: "192.168.10.101",
-  port: 3306,
-  username: "boarduser",
-  password: "boardpass",
-  database: "boarddb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   // replication: {
   //   master: {
   //     host: "192.168.10.101",
